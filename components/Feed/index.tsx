8d0ef4062b3354a3e1067d0568fbf2a95fdcb541
Adam Turner <hello@adamrichardturner.dev>
2024-01-13 04:53:42 +0000

    Tidied logic

--- components/Feed/index.tsx
+++ components/Feed/index.tsx
@@ -4,20 +4,31 @@ import { useState } from 'react'
 import Notification from '@/components/Notification'
 import usersData from '@/utils/users.json'
 
-export default function Home() {
-  // Step 1: Use useState to manage the state of the users data
+export default function Page() {
   const [users, setUsers] = useState(usersData)
+  const [counter, setCounter] = useState(
+    usersData.filter((user) => !user.seen).length
+  )
 
   const markAllAsRead = () => {
-    // Step 2: Update the seen property for each user
     const updatedUsers = users.map((user) => ({ ...user, seen: true }))
-    setUsers(updatedUsers) // Update the state
+    setUsers(updatedUsers)
+    setCounter(0)
   }
 
   const notificationList = users
     .sort((a, b) => a.id - b.id)
     .map((user, index) => {
-      return <Notification key={user.id} user={user} i={index} />
+      return (
+        <Notification
+          key={user.id}
+          user={user}
+          i={index}
+          setCounter={setCounter}
+          counter={counter}
+          viewed={false}
+        />
+      )
     })
 
   return (
@@ -27,16 +38,22 @@ export default function Home() {
           <div className='flex flex-row space-x-2 items-center'>
             <h2 className='font-bold text-xl'>Notifications</h2>
             <div className='bg-primary-blue px-2 rounded font-bold text-white'>
-              {users.filter((user) => user.seen).length}
+              {counter}
             </div>
           </div>
 
           {/* Step 3: Attach the function to the onClick event of the button */}
-          <div onClick={markAllAsRead}>
-            <p className='text-gray-500 hover:text-primary-blue cursor-pointer'>
-              Mark all as read
-            </p>
-          </div>
+          {counter > 0 && (
+            <div onClick={markAllAsRead}>
+              <p
+                className={`${
+                  counter > 0 ? 'hidden' : ''
+                }text-gray-500 hover:text-primary-blue cursor-pointer`}
+              >
+                Mark all as read
+              </p>
+            </div>
+          )}
         </div>
         <div className='space-y-2'>{notificationList}</div>
       </section>
--- components/Notification/index.tsx
+++ components/Notification/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, MouseEventHandler, useState } from 'react'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 
@@ -16,26 +16,40 @@ export interface UserNotification {
 interface NotificationProps {
   user: UserNotification
   i: number
+  viewed: boolean
+  setCounter: Function
+  counter: number
 }
 
-const Notification: FC<NotificationProps> = ({ user, i }) => {
-  // State to track if the images have loaded
-  const [imagesLoaded, setImagesLoaded] = useState(false)
-
-  // Event handler to set imagesLoaded to true
-  const handleImageLoad = () => {
-    setImagesLoaded(true)
+const Notification: FC<NotificationProps> = ({
+  viewed,
+  setCounter,
+  user,
+  counter,
+  i,
+}) => {
+  const [userSeen, setUserSeen] = useState(user.seen)
+  const handleClick = () => {
+    if (!userSeen) {
+      setCounter((prev: number) => prev - 1)
+      setUserSeen(true) // Mark as seen
+    }
   }
+
   return (
     <motion.article
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 0.125, delay: i * 0.05 }} // Adjust delay based on index
-      className={`${
-        !user.seen ? 'bg-neutral-veryLightGrayishBlue' : ''
-      } notification w-full flex flex-col py-4 pl-4 pr-6 rounded-lg leading-tight`}
+      transition={{ duration: 0.125, delay: i * 0.05 }}
     >
-      <div className='flex flex-row md:flex-row items-start space-x-3 space-y-0'>
+      <div
+        className={`${
+          userSeen
+            ? 'bg-white pointer-events-none cursor-default'
+            : 'bg-neutral-veryLightGrayishBlue cursor-pointer'
+        } flex flex-row md:flex-row items-start space-x-3 space-y-0 p-4`}
+        onClick={handleClick}
+      >
         <div
           style={{
             minWidth: '40px',
@@ -51,29 +65,32 @@ const Notification: FC<NotificationProps> = ({ user, i }) => {
           />
         </div>
 
-        <div className='flex-grow flex flex-col items-start'>
+        <div onClick={() => handleClick}>
           <div className='inline space-x-0.5'>
-            <span className='font-bold text-neutral-veryDarkBlue hover:text-primary-blue hover:cursor-pointer'>
+            <span className='font-bold text-neutral-veryDarkBlue hover:text-primary-blue'>
               {user.name}{' '}
             </span>
             <span className='text-neutral-grayishBlue font-normal'>
               {user.notification}{' '}
             </span>
             {user.post && (
-              <span className='font-bold text-neutral-grayishBlue cursor-pointer'>
+              <span className='font-bold text-neutral-grayishBlue'>
                 {user.post}{' '}
               </span>
             )}
             {user.groups && (
-              <span className='font-bold text-primary-blue cursor-pointer'>
+              <span className='font-bold text-primary-blue'>
                 {user.groups}{' '}
               </span>
             )}
             {!user.seen && (
               <div className='inline-block'>
-                <span className='flex flex-row items-center h-full'>
+                <span
+                  className={`${
+                    userSeen ? 'hidden' : ''
+                  } seen-indicator flex flex-row items-center h-full`}
+                >
                   <span
-                    className='seen-indicator'
                     style={{
                       display: 'inline-block',
                       width: '8px',
@@ -91,7 +108,7 @@ const Notification: FC<NotificationProps> = ({ user, i }) => {
               {user.timestamp}
             </div>
             {user.quote && (
-              <div className='w-full border-neutral-grayishBlue border-[0.5px] mt-2 p-4 rounded cursor-pointer hover:bg-neutral-lightGrayishBlue1'>
+              <div className='w-full border-neutral-grayishBlue border-[0.5px] mt-2 p-4 rounded hover:bg-neutral-lightGrayishBlue1'>
                 <p className='text-neutral-grayishBlue'>{user.quote}</p>
               </div>
             )}
